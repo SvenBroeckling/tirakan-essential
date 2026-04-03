619625de909195e60730e7bbde882b396a849622
Sven Bröckling <sven@broeckling.de>
2026-04-03 13:23:00 +0200

    Added index

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ export TEXMFVAR="$(pwd)/.texlive-cache"
 
 clean() {
     rm -f main.aux main.bcf main.blg main.fdb_latexmk main.fls main.log main.out main.pdf main.run.xml main.toc main.xdv
+    rm -f main.idx main.ilg main.ind
     rm -f sheet.aux sheet.fdb_latexmk sheet.fls sheet.log sheet.out sheet.pdf sheet.xdv
     rm -f chapters/*.aux
 }
@@ -17,6 +18,7 @@ build() {
     lualatex sheet.tex
     lualatex sheet.tex
     lualatex main.tex
+    makeindex main.idx
     lualatex main.tex
     atril main.pdf &
 }
