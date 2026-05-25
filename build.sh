46e375b2d141fd572cfb8ee256b66d2b06eac546
Sven <sven@nimbostratus.de>
2026-05-25 17:21:36 +0200

    Added npc sheet

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ clean() {
     rm -f main.aux main.bcf main.blg main.fdb_latexmk main.fls main.log main.out main.pdf main.run.xml main.toc main.xdv
     rm -f main.idx main.ilg main.ind
     rm -f sheet.aux sheet.fdb_latexmk sheet.fls sheet.log sheet.out sheet.pdf sheet.xdv
+    rm -f npc_sheet.aux npc_sheet.fdb_latexmk npc_sheet.fls npc_sheet.log npc_sheet.out npc_sheet.pdf npc_sheet.xdv
     rm -f chapters/*.aux
 }
 
@@ -17,11 +18,15 @@ build() {
     clean
     lualatex sheet.tex
     lualatex sheet.tex
+    lualatex npc_sheet.tex
+    lualatex npc_sheet.tex
     lualatex main.tex
     makeindex main.idx
     lualatex main.tex
     lualatex main.tex
-    atril main.pdf &
+    firefox main.pdf &
+    firefox sheet.pdf &
+    firefox npc_sheet.pdf &
 }
 
 action="${1:-build}"
